d8b626f5d3646c5822c164f5470c4c0e37ad0829
GioMai24 <giovanniandrea.maida@studenti.unipd.it>
2026-04-30 18:10:39 +0200

    3 streams

--- benchCuda.sh
+++ benchCuda.sh
@@ -2,13 +2,15 @@
 export LC_ALL=C TIMEFORMAT='%R %U %S'
 
 date
-for ((rows=512; rows<=16384; rows*=2))
-	do 
+#for ((rows=512; rows<=16384; rows*=2))
+#	do 
 		for it in {1..100}
 		do 
-			echo -ne "N:" $rows "- iteration:" $it '\r'
-			echo $rows $( (time ./bench.out $rows) 2>&1 ) >> logs/cufft/times.csv
+#			echo -ne "N:" $rows "- iteration:" $it '\r'
+#			echo $rows $( (time ./bench.out $rows) 2>&1 ) >> logs/multImages/3Streams.csv
+			echo -ne "N:" 4096 "- iteration:" $it '\r'
+			echo 4096 $( (time ./bench.out 4096) 2>&1 ) >> logs/multImages/3Streams.csv
 		done
-done
+#done
 echo "I am done."
 date
